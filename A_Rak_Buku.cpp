060d168531fd51551d49c11ca2ccfda250866ed4
setiAjiid <133765568+setiAjiid@users.noreply.github.com>
2023-05-31 21:28:52 +0700

    Add files via upload

---
+++ A_Rak_Buku.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n,rak, arr[20000], sum = 0, cek = 0;
+    memset(arr,0,sizeof(arr));
+    cin>>n>>rak;
+    for (int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int ukuran = sizeof(arr)/sizeof(arr[0]);
+    sort(arr, arr+ukuran, greater<int>());
+    for (int i=0;i<n;i++){
+        sum += arr[i];
+        cek++;
+        if (sum>=rak){
+            break;
+        }
+    }
+    cout<<cek<<endl;
+}
